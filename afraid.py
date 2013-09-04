f29cd1eab121862b3743b24041b8349d2a599891
Pedram Navid <pedram@pedramnavid.com>
2013-09-04 19:48:25 -0400

    first commit

--- afraid.py
+++ afraid.py
@@ -1,11 +1,24 @@
-import urllib2
-import re
+import codecs
 import socket
+import re
+import urllib.request
+import configparser
+import os
+import sys
 
+def get_config(fname):
+    config = configparser.ConfigParser()
+    config['Settings'] = { 'apikey': 'YOURAPIKEY',
+                          'domain': 'YOURDOMAIN.afraid.org'}
+    if not os.path.exists(fname):
+        with open(fname, 'w') as configfile:
+            config.write(configfile)
+    config.read(fname)
+    return config
 
 def get_ip():
     url = 'http://whatsmyip.net'
-    result = urllib2.urlopen(url).read()
+    result = codecs.decode(urllib.request.urlopen(url).read())
     ip = re.findall('<input type="text" value="([0-9.]+)', result)[0]
     return ip
 
@@ -16,10 +29,16 @@ def register(ip, domain, apikey):
     if get_registered(domain) != ip:
         updateurl = 'http://freedns.afraid.org/dynamic/update.php?'
         url = updateurl + apikey
-        result = urllib2.urlopen(url).read()
+        result = codecs.decode(urllib.request.urlopen(url).read())
         return result
+    else:
+        return "IP {} hasn't changed, nothing done.".format(ip)
 
 if __name__ == '__main__':
-    apikey='Nkg1OHBqdW9QSkxSdGlINVhXNEo6ODUxMzI3OQ=='
-    domain = 'multiphrenic.mooo.com'
+    config = get_config('afraid.rc')
+    if config['Settings']['apikey']=='YOURAPIKEY':
+        print('Config not set. Please enter your api key and domain in the afraid.rc file')
+        sys.exit(1)
+    apikey = config['Settings']['apikey']
+    domain = config['Settings']['domain']
     print(register(get_ip(), domain, apikey))
